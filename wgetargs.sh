59202188c0643bf1f6ec833ea872ea0cbcf26dda
Paul Musselman <40570775+ghostmech007@users.noreply.github.com>
2018-08-22 14:48:47 -0400

    Update wgetargs.sh

--- wgetargs.sh
+++ wgetargs.sh
@@ -1,1 +1,1 @@
-wget -r -nH -np -nc --cache=off -e robots=off --reject m3u,cue,index,html,tmp,txt --user-agent='Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0' --limit-rate=10M --wait 1
+wget -r -nH -np -nc --cache=off -e robots=off --reject m3u,cue,index*,html,tmp,txt --user-agent='Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0' --limit-rate=10M --wait 1
