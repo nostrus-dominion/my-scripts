d34ed3b9a6a7c7138bf17286a2b444c408c5d8e5
Paul M <40570775+ghostmech007@users.noreply.github.com>
2019-02-04 18:29:09 -0500

    Update ffmpeg-flac.bash

--- ffmpeg-flac.bash
+++ ffmpeg-flac.bash
@@ -1,5 +1,3 @@
-## FOR CORUSCANT
+#/bin/sh
 for f in *.flac; do sudo ffmpeg -i "$f" -acodec libmp3lame -ab 320k "${f%.flac}.mp3"; done && sudo chown plex:plex * && sudo chmod 644 * && find . -name "*.flac" -exec rm -rf {} \;
 
-## FOR TORRENT
-for f in *.flac; do sudo ffmpeg -i "$f" -acodec libmp3lame -ab 320k "${f%.flac}.mp3"; done && find . -name "*.flac" -exec rm -rf {} \;
