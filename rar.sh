3dc101956ec8e99e25798b297e3fe636ca7e2116
Paul M <40570775+ghostmech007@users.noreply.github.com>
2019-04-02 11:18:04 -0400

    Update rar.sh

--- rar.sh
+++ rar.sh
@@ -1,3 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-find ./ -name '*.rar' -execdir 7z e {} +
+# Used to extract archives from Deluge
+
+
+formats=(zip rar)
+commands=([zip]="7z e" [rar]="7z e")
+extraction_subdir='extracted'
+
+torrentid=$1
+torrentname=$2
+torrentpath=$3
+
+cd "${torrentpath}"
+for format in "${formats[@]}"; do
+    while read file; do 
+        cd "$(dirname "$file")"
+        file=$(basename "$file")
+        ${commands[$format]} "$file"
+    done < <(find "$torrentpath/$torrentname" -iname "*.${format}" )
+done
