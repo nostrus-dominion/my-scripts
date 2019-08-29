67a1835ef7000feaf368fcda5112f559cf69c039
Paul M <40570775+ghostmech007@users.noreply.github.com>
2019-08-29 10:33:14 -0400

    Update wget-args.sh

--- wget-args.sh
+++ wget-args.sh
@@ -1,1 +1,1 @@
-wget -r -nH -np -nc --cache=off -e robots=off --reject m3u,cue,index*,html,tmp,txt --user-agent='Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0' --limit-rate=10M --wait 1
+wget -r -nH -np -nc --cache=off -e robots=off --reject m3u,cue,index*,html,tmp,txt --user-agent='Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' --limit-rate=10M --wait 1
