9341879d72d0ccb1ad71e1886e47572439ba38de
Paul M <40570775+ghostmech007@users.noreply.github.com>
2021-02-07 17:50:16 -0500

    Update ffmerge.bash

--- ffmerge.bash
+++ ffmerge.bash
@@ -9,7 +9,7 @@
 # This script is designed to merge all files of the same codec and extension (ex. mp3, m4a, mp4, mkv)
 # in the working directory using FFMPEG. It is required that all the files be from the same source,
 # with the same containers, codec, dimensions (etc). Files from a different source or codec will fail
-# and cannot be merged. Additionally, you will have to manually clean up the directory after this runs.
+# and cannot be merged.
 
 echo -e "WARNING! PLEASE BE SURE THAT ALL MEDIA IS OF THE SAME CODEC/CONTAINER!"
 echo -e "THIS SCRIPT WILL NOT WORK IF YOUR MEDIA IS FROM DIFFERENT SOURCES!"
@@ -20,14 +20,18 @@ echo -e "Please enter the file extension of your media:"
 
 read -r EXT
 
+echo -e "Please enter the name of the output:"
+
+read -r OUT
+
 echo -e "Five second countdown to [CTRL-C] to cancel"
 
 for i in {5..1};do echo -n "$i." && sleep 1; done
 
 for f in ./*.$EXT; do echo "file '$f'" >> list.txt; done
 
-ffmpeg -f concat -safe 0 -i list.txt -c copy "$BOOK.mp3"
+ffmpeg -f concat -safe 0 -i list.txt -c copy "$OUT.$EXT"
 
 rm list.txt
 
-echo "DONE!"
+echo -e "DONE!"
