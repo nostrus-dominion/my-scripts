3227d4795a35bf09a21f5694280da2908dabb3ea
Paul M <40570775+ghostmech007@users.noreply.github.com>
2021-05-10 15:19:55 -0400

    Update 99bottles.bash

--- 99bottles.bash
+++ 99bottles.bash
@@ -1,7 +1,33 @@
-<html>
-<head><title>403 Forbidden</title></head>
-<body bgcolor="white">
-<center><h1>403 Forbidden</h1></center>
-<hr><center>nginx</center>
-</body>
-</html>
+#!/bin/bash
+# Bourne Again shell version of 99 Bottles
+# Dave Plonka - plonka@carroll1.cc.edu
+# Edited by Paul Musselman so it goes VERY slow
+
+typeset -i n=99
+typeset bottles=bottles
+typeset no
+
+while [ 0 != $[ n ] ]
+do
+   echo "${n?} ${bottles?} of beer on the wall,"
+   sleep 1.5
+   echo "${n?} ${bottles?} of beer,"
+   sleep 1.5
+   echo "take one down, pass it around,"
+   sleep 1.5
+   n=n-1
+   case ${n?} in
+   0)
+      no=no
+      bottles=${bottles%s}s
+      ;;
+   1)
+      bottles=${bottles%s}
+      ;;
+   esac
+   echo "${no:-${n}} ${bottles?} of beer on the wall."
+   sleep 2.5
+   echo
+done
+
+exit
