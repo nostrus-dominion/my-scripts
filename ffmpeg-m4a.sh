8087fb4c39ef47e609f3e20c7ef5139f68a33751
Paul M <40570775+ghostmech007@users.noreply.github.com>
2021-06-01 14:50:28 -0400

    Update ffmpeg-m4a.sh

--- ffmpeg-m4a.sh
+++ ffmpeg-m4a.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-for f in *.m4a; do sudo ffmpeg -i "$f" -acodec libmp3lame -ab 320k "${f%.m4a}.mp3"; done && sudo chown plex:plex * && sudo chmod 644 * && find . -name "*.m4a" -exec rm -rf {} \;
+
+find . -name "*.m4a" -exec bash -c 'for f'; do ffmpeg -i "$f" -acodec libmp3lame -b:a 320k "${f%.*}.mp3"; done' -- {} + 
