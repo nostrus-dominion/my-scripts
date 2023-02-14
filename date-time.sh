9c33699fe4f6e1b2a955c666ddef2d264d7ddb86
Paul M <40570775+ghostmech007@users.noreply.github.com>
2023-02-14 10:48:07 -0500

    Update date-time.sh
    Updated script to include progress of changes and number of files touched.

--- date-time.sh
+++ date-time.sh
@@ -3,7 +3,17 @@
 echo "Enter the desired date and time (YYYY-MM-DD HH:MM:SS):"
 read target_datetime
 
+total_items=$(find . -type f -o -type d | wc -l)
+current_item=0
+
 find . -type f -o -type d | while IFS= read -r item
+
 do
+  current_item=$((current_item + 1))
+  percentage_complete=$((100 * current_item / total_items))
+  echo -ne "\rChanging creation date of items... $percentage_complete% complete"
+
   touch -d "$target_datetime" "$item"
 done
+
+echo -ne "\n"
