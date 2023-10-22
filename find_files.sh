47aaa303ac1df71f664fdbc88b68f2be50c9aae4
The Nostrus Dominion <admin@nostrus-dominion.co>
2023-10-22 15:58:36 -0400

    Update find_files.sh

--- find_files.sh
+++ find_files.sh
@@ -4,22 +4,26 @@ while true; do
     # Prompt the user for file name pattern
     read -p "Enter file name pattern (or 'exit' to quit, or 'clear' to clean screen): " pattern
 
-    # Check if the user wants to exit
-
+    # Check user input
     if [ "$pattern" == "clear" ]; then
             clear
-            echo "Cleared screen. Enter file name pattern (or 'exit' to quit): " pattern
+            echo "Cleared screen. Enter file name pattern (or 'exit' to quit): "
             continue
-    fi      
+    fi
 
     if [ "$pattern" == "exit" ]; then
         echo "Exiting the script. Goodbye!"
         exit 0
     fi
 
+    if [ -z "$pattern" ]; then
+            echo "ERROR! Input cannot be empty. Please try again."
+            continue
+    fi
+
     # Use find command to search for files matching the pattern in the current directory
-    matching_files=$(find . -type f -iname "*$pattern*")
-    
+    matching_files=$(find ./ -type f -iname "*$pattern*")
+
     if [ -z "$matching_files" ]; then
         #If no matching file found prints error message
         echo -e "\e[31mNo matching file found! Try again!\e[0m"
@@ -29,7 +33,7 @@ while true; do
         echo "$matching_files" | sort
 
     fi
-    
+
     # Add a newline for better readability
     echo
 
