5b578e0258505b5ce2d83acbd129793409a68d39
The Nostrus Dominion <admin@nostrus-dominion.co>
2023-10-21 20:43:34 -0400

    Create find_files.sh

---
+++ find_files.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+while true; do
+    # Prompt the user for file name pattern
+    read -p "Enter file name pattern (or 'exit' to quit, or 'clear' to clean screen): " pattern
+
+    # Check if the user wants to exit
+
+    if [ "$pattern" == "clear" ]; then
+            clear
+            echo "Cleared screen. Enter file name pattern (or 'exit' to quit): " pattern
+            continue
+    fi      
+
+    if [ "$pattern" == "exit" ]; then
+        echo "Exiting the script. Goodbye!"
+        exit 0
+    fi
+
+    # Use find command to search for files matching the pattern in the current directory
+    matching_files=$(find . -type f -iname "*$pattern*")
+    
+    if [ -z "$matching_files" ]; then
+        #If no matching file found prints error message
+        echo -e "\e[31mNo matching file found! Try again!\e[0m"
+    else
+        echo
+        echo "Found the following files matching '$pattern':"
+        echo "$matching_files" | sort
+
+    fi
+    
+    # Add a newline for better readability
+    echo
+
+done
