18fd6df9b7619ec3b73924b339107b79a4732cc8
The Nostrus Dominion <admin@nostrus-dominion.co>
2023-10-30 11:01:24 -0400

    Update filetypes.bash

--- filetypes.bash
+++ filetypes.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-#This script shows all filetypes from this directory
-#into all subdirectories. I actually have no idea
-#why I made this stupid waste of compute power.
+# Find all filetypes, count them, and format output in multiple columns
+filetypes=$(find . -type f | grep -i -E -o "\.\w*$" | sort | uniq -c | awk '{printf "%-17s %s\n", $2, $1}')
+total_files=$(find . -type f | wc -l)
 
-find . -type f | grep -i -E -o "\.\w*$" | sort | uniq -c
+# Display filetypes in columns
+echo -e "Filetype\tCount\n$filetypes"
+
+# Display total number of files found
+echo -e "\nTotal number of files found: $total_files"
+echo -e
