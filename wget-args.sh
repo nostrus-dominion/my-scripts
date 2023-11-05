4fefa57d4a80edcb97c32a9b7374e59b1a03cf56
The Nostrus Dominion <admin@nostrus-dominion.co>
2023-11-04 23:50:36 -0400

    Removed wget-args.sh

--- wget-args.sh
+++
@@ -1,1 +0,0 @@
-wget -r -nH -np -nc --cache=off -e robots=off --reject m3u,cue,index*,html,tmp,txt --user-agent='Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' --limit-rate=10M --wait 1
