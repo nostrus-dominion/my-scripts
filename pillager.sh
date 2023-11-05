696a00d21ea3c6756973348af2d46ee4d427f89b
The Nostrus Dominion <admin@nostrus-dominion.co>
2023-11-04 23:41:50 -0400

    Overall updates

--- pillager.sh
+++ pillager.sh
@@ -5,9 +5,12 @@
 ## Copyright: (c) 2023
 
 # Color variables
-red='\033[0;31m'
 green='\033[0;32m'
-yellow='\033[0;33m'
+yellow='\033[1;33m'
+red='\033[0;31m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+brown='\033[0;33m'
 cyan='\033[0;36m'
 reset='\033[0m' # No Color
 
