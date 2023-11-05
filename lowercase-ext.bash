105e9f53b4ddda14f7f4e632631753a2f32d3f08
The Nostrus Dominion <admin@nostrus-dominion.co>
2023-11-04 23:47:53 -0400

    Again removing lowercase-ext.bash

--- lowercase-ext.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#This script was taken from a thread on stackoverflow.
-#You can find it here:
-#https://stackoverflow.com/questions/11818408/convert-all-file-extensions-to-lower-case
-
-find . -name '*.*' -type f -exec bash -c 'base=${0%.*} ext=${0##*.} a=$base.${ext,,}; [ "$a" != "$0" ] && mv -- "$0" "$a"' {} \;
