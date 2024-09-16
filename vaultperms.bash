dbfbbe1e1791d6573fd6776a7b07e2b149c0ac8b
Nostrus Admin <admin@nostrus-dominion.co>
2024-09-15 21:20:46 -0400

    Updated vaultperms.bash

--- vaultperms.bash
+++ vaultperms.bash
@@ -2,4 +2,6 @@
 
 # Version 0.4
 
-find /mnt/thevault -path '/mnt/thevault/Nginx-Fancyindex-Theme-dark' -prune -o \( ! -perm 775 -type d \) -o \( ! -perm 755 -type f \) -exec chmod -- {} +
+# Prune the specific path
+find /mnt/thevault -path '/mnt/thevault/Nginx-Fancyindex-Theme-dark' -prune -o \( -type d \! -perm 775 \) -print0 | xargs -0 chmod 775
+find /mnt/thevault -path '/mnt/thevault/Nginx-Fancyindex-Theme-dark' -prune -o \( -type f \! -perm 755 \) -print0 | xargs -0 chmod 755
