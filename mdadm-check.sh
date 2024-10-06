9fd76f3eb2e1a6d05c8f51352b5d7f902a7d1255
Nostrus Admin <admin@nostrus-dominion.co>
2024-10-06 18:33:04 -0400

    Updated mdadm-check.sh

--- mdadm-check.sh
+++ mdadm-check.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
-## Version 0.2
+## Version 0.5
 ## License: Open Source GPL
 ## Copyright: (c) 2023
 
+## COLORS
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+reset=$(tput sgr0) # No color
+
 # Check if the script is run as root
 if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit 1
+    echo ""
+    echo -e "${red}Please run as root!${reset}"
+    echo ""
+    exit 1
 fi
+
+# Function to display the current check state and progress with color formatting
+display_status() {
+  local status_output
+  status_output=$(mdadm --detail /dev/md0)
+
+  echo "$status_output" | while read -r line; do
+    if echo "$line" | grep -q "State"; then
+      if echo "$line" | grep -q "clean, checking"; then
+        echo "${green}$line${reset}" # Green color for "clean, checking"
+      else
+        echo "$line"
+      fi
+    elif echo "$line" | grep -q -E 'Rebuild Status|Resync Status'; then
+      echo "$line"
+    fi
+  done
+}
+
+# Display the current check state and progress
+echo "Current status of /dev/md0:"
+display_status
+echo ""
 
 #Execute writing to sync_action
 while true; do
