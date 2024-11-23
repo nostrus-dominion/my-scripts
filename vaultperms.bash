ee382fa1c59bdce25bc6d3094d6241a7047a92aa
Nostrus Admin <admin@nostrus-dominion.co>
2024-11-23 10:58:00 -0500

    Fixed direcotyr

--- vaultperms.bash
+++ vaultperms.bash
@@ -2,4 +2,4 @@
 
 # Version 0.6
 
-sudo find /mnt/plex -path '/mnt/thevault/Nginx-Fancyindex-Theme-dark' -prune \( ! -user pmusselman -o ! -group pmusselman -o \( -type d -a ! -perm 775 \) -o \( -type f -a ! -perm 755 \) \) -exec chown -v pmusselman:pmusselman {} + -exec chmod -v u+rwX,g+rwX,o+rX {} + -exec test -d {} \; -exec chmod -v 775 {} + -exec test -f {} \; -exec chmod -v 755 {} +
+sudo find /mnt/thevault -path '/mnt/thevault/Nginx-Fancyindex-Theme-dark' -prune \( ! -user pmusselman -o ! -group pmusselman -o \( -type d -a ! -perm 775 \) -o \( -type f -a ! -perm 755 \) \) -exec chown -v pmusselman:pmusselman {} + -exec chmod -v u+rwX,g+rwX,o+rX {} + -exec test -d {} \; -exec chmod -v 775 {} + -exec test -f {} \; -exec chmod -v 755 {} +
