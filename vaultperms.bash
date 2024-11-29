07ac510a233171d8ceeec0a08aea4d557821f707
Nostrus Admin <admin@nostrus-dominion.co>
2024-11-29 12:03:51 -0500

    Updated vaultperms.bash

--- vaultperms.bash
+++ vaultperms.bash
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-# Version 0.6
+# Version 0.7
 
-sudo find /mnt/thevault/ -path '/mnt/thevault/Nginx-Fancyindex-Theme-dark' -prune -o \( ! -user pmusselman -o ! -group pmusselman -o \( -type d -a ! -perm 775 \) -o \( -type f -a ! -perm 755 \) \) -exec chown -v pmusselman:pmusselman {} + -exec chmod -v u+rwX,g+rwX,o+rX {} + -exec test -d {} \; -exec chmod -v 775 {} + -exec test -f {} \; -exec chmod -v 755 {} +
+# Default directory
+DIRECTORY="/mnt/thevault/"
+
+# Check for -pwd flag
+if [ "$1" == "-pwd" ]; then
+  DIRECTORY=$(pwd)
+fi
+
+sudo find "$DIRECTORY" -path "$DIRECTORY/Nginx-Fancyindex-Theme-dark" -prune -o \
+  \( ! -user pmusselman -o ! -group pmusselman -o \
+  \( -type d -a ! -perm 775 \) -o \
+  \( -type f -a ! -perm 755 \) \) \
+  -exec chown -v pmusselman:pmusselman {} + \
+  -exec chmod -v u+rwX,g+rwX,o+rX {} + \
+  -exec test -d {} \; -exec chmod -v 775 {} + \
+  -exec test -f {} \; -exec chmod -v 755 {} +
