27e41e50a639db69b5bcf731bb6e45bba8f00ae3
The Nostrus Dominion <admin@nostrus-dominion.co>
2025-04-12 10:00:22 -0400

    Update plexperms.bash

--- plexperms.bash
+++ plexperms.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Version 0.6
+# Version 0.7
 # License: GPL
 
-sudo find /mnt/plex \( ! -user plex -o ! -group plex -o \( -type d -a ! -perm 775 \) -o \( -type f -a ! -perm 755 \) \) -exec chown -v plex:plex {} + -exec chmod -v u+rwX,g+rwX,o+rX {} + -exec test -d {} \; -exec chmod -v 775 {} + -exec test -f {} \; -exec chmod -v 755 {} +
-
+sudo find . \( ! -user plex -o ! -group plex -o \( -type d -a ! -perm 775 \) -o \( -type f -a ! -perm 755 \) \) \
+  -exec chown -v plex:plex {} + \
+  -exec chmod -v u+rwX,g+rwX,o+rX {} + \
+  -exec test -d {} \; -exec chmod -v 775 {} + \
+  -exec test -f {} \; -exec chmod -v 755 {} +
