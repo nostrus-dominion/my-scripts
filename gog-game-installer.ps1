5fe7e4d7d2247af0f46e6ff551a4e321e5460612
The Nostrus Dominion <admin@nostrus-dominion.co>
2025-09-19 15:18:57 -0400

    Update gog-game-installer.ps1

--- gog-game-installer.ps1
+++ gog-game-installer.ps1
@@ -1,9 +1,9 @@
-# Set your folder path
-$folder = "C:\PATH\TO\GAME\FOLDER\"
+# this script is desinged for gog games with multiple dlcs
 
+$folder = Split-Path -Parent $MyInvocation.MyCommand.Path
 $installers = Get-ChildItem -Path $folder -Filter *.exe | Sort-Object Name
 
 foreach ($installer in $installers) {
-    Write-Host "Installing $($installer.Name)..."
-    Start-Process -FilePath $installer.FullName -ArgumentList "/VERYSILENT", "/NORESTART", "/SUPPRESSMSGBOXES" -Wait
+    Write-Host "Installing $($installer.Name) to $folder..."
+    Start-Process -FilePath $installer.FullName -ArgumentList "/VERYSILENT", "/NORESTART", "/SUPPRESSMSGBOXES", "/DIR=`"$folder`"" -Wait
 }
