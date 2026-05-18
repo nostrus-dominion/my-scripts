1983792246f008bc9a27e200f7b39777c761bdc5
Nostrus Admin <admin@nostrus-dominion.co>
2026-05-18 09:35:23 -0400

    Update files

--- motd.bash
+++ motd.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-## Version 0.2
+## Version 0.3
 ## License: Open Source GPL
-## Copyright: (c) 2023
+## Copyright: (c) 2026
+
+if [ $(id -u) -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
 
 for f in /etc/update-motd.d/* ; do
     if [ -x "$f" ]; then
